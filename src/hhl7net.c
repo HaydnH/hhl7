1cfcfc7d8df77d374df08ba8af33e7419deaa1c4
Haydn Haines <hhaines@nervecentresoftware.com>
2024-02-04 18:08:57 +0000

    Removed 25 argument limit for responder

--- src/hhl7net.c
+++ src/hhl7net.c
@@ -600,7 +600,7 @@ static struct Response *checkResponse(char *msg, char *sIP, char *sPort, char *t
     handleError(LOG_WARNING, "Could not allocate memory to create a response", 1, 0, 1);
     free(resp->sendArgs);
     free(resp);
-    return(NULL);
+    return(responses);
   }
 
   for (m = 0; m < mCount; m++) {
