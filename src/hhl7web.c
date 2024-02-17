94b1843b3b78ec70764971205e5c0a997ae51e7b
Haydn Haines <hhaines@nervecentresoftware.com>
2024-02-17 23:48:15 +0000

    Removed debug comment

--- src/hhl7web.c
+++ src/hhl7web.c
@@ -501,8 +501,6 @@ static enum MHD_Result getTemplateList(struct Session *session,
   char errStr[300] = "";
   int fCount = 0;
 
-printf("U: %s\n", url);
-
   char *dirOpts = malloc(38);
   char *tempOpts = malloc(1);
   if (dirOpts == NULL || tempOpts == NULL) {
